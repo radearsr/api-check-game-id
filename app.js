cc276789ef9b10a15e69465beceb890576c65658
radearsr <radea.surya4@gmail.com>
2024-03-07 09:49:45 +0700

    update dotenv

--- app.js
+++ app.js
@@ -3,20 +3,22 @@ const timeout = require("express-timeout-handler");
 const apiRoutes = require("./routes/apiRoutes");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.SERVER_PORT || 3000;
 
 app.use(express.json());
 
 app.use("/api", apiRoutes);
 
-app.use(timeout.handler({
-  timeout: 30000,
-  onTimeout: (req, res) => {
-    console.log("TEST");
-    res.status(503).send("Service unavailable. please retry");
-  },
-  disable: ["write", "setHeaders", "send", "json", "end"],
-}));
+app.use(
+  timeout.handler({
+    timeout: 30000,
+    onTimeout: (req, res) => {
+      console.log("TEST");
+      res.status(503).send("Service unavailable. please retry");
+    },
+    disable: ["write", "setHeaders", "send", "json", "end"],
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running with port ${PORT}`);
